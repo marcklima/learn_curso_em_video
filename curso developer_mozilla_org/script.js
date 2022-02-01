f4b90efb55d791b744f60e3e2810f2efb15ba343
v1 <marcklima@gmail.com>
2022-02-01 18:59:00 -0300

    v5

---
+++ curso developer_mozilla_org/script.js
@@ -0,0 +1,15 @@
+var numeroAleatorio= Math.floor(Math.random() * 100) + 1;
+
+var palpites = document.querySelector('.palpites');
+var ultimoResultado = document.querySelector('.ultimoResultado');
+var baixoOuAlto = document.querySelector('.baixoOuAlto');
+
+var envioPalpite = document.querySelector('.envioPalpite');
+var campoPalpite = document.querySelector('.campoPalpite');
+
+var contagemPalpites = 1;
+var botaoReinicio;
+
+function conferirPalpite() {
+  alert('Eu sou um placeholder');
+}
